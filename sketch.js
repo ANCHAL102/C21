52976392519ebeff59975c96b2f900cd6943c1b9
ANCHAL102 <80636070+ANCHAL102@users.noreply.github.com>
2021-03-28 21:01:24 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,27 @@
+var fixedRect,movingRect;
+var box
+function setup() {
+  createCanvas(800,400);
+  fixedRect= createSprite(200, 200, 50, 80);
+  movingRect= createSprite(400, 200, 80, 30);
+box= createSprite(100,100,50,50)
+}
+
+function draw() {
+  background(255,255,255);  
+  movingRect.x=World.mouseX;
+  movingRect.y=World.mouseY;
+
+ if(isTouching(movingRect,box))
+{
+  movingRect.shapeColor="red";
+  box.shapeColor="red";
+}
+else{
+  movingRect.shapeColor="green";
+  box.shapeColor="green";
+}
+
+  drawSprites();
+}
+
